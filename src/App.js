63d9f5095a051e0257d7a5bd69f3f59c71a92673
ngoctienle <ngoctien@bincg.com>
2022-11-03 11:55:54 +0700

    feat(*): initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+const App = () => {
+  return (
+    <Fragment>
+      <header className="font-medium">ABC</header>
+      <main>ABC</main>
+    </Fragment>
+  )
+}
+
+export default App
